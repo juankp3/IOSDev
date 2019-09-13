8cda153ae9defecd172c95a00a994bc841a494df
Juan Kuga <juankp3@gmail.com>
2019-09-13 08:10:24 -0500

    Iniciando proyecto

---
+++ Tablas/Tablas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tablas
+//
+//  Created by juan kuga on 9/13/19.
+//  Copyright © 2019 Juan Kuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
