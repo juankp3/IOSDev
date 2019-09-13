4451be4587d057cf8c251ef326288b6447e926f7
Juan Kuga <juankp3@gmail.com>
2019-09-13 08:41:26 -0500

    Tabla estatica

---
+++ Tablas/Tablas/NewPokemonTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewPokemonTableViewController.swift
+//  Tablas
+//
+//  Created by juan kuga on 9/13/19.
+//  Copyright © 2019 Juan Kuga. All rights reserved.
+//
+
+import UIKit
+
+class NewPokemonTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+   
+
+}
--- Tablas/Tablas/ViewController.swift
+++ Tablas/Tablas/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func getPokemonList(segue : UIStoryboardSegue) {
+        
+    }
 
 
 }
