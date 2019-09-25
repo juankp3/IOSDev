ae5f96613e3fb007fa2a5f77f343400081a7a2b7
Juan Kuga <juankp3@gmail.com>
2019-09-24 23:37:24 -0500

    iniciando pokedex

---
+++ pokedex/pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by juan kuga on 9/24/19.
+//  Copyright © 2019 Juan Kuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
